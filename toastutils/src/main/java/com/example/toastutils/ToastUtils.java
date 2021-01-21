da788cde424cca142bf4e71f806cb8de200fb417
DESKTOP-CHSEI3M\Administrator <kiwilss@163.com>
2021-01-21 16:24:22 +0800

    Initial commit

---
+++ toastutils/src/main/java/com/example/toastutils/ToastUtils.java
@@ -0,0 +1,19 @@
+package com.example.toastutils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * author : Administrator
+ * e-mail : kiwilss@163.com
+ * time   : 2021/01/21
+ * desc   : http://localhost:8081/repository/MavenTest/
+ * @author Administrator
+ */
+public class ToastUtils {
+
+    public static void testToast(Context context){
+        Toast.makeText(context, "hello word 1.0.1", Toast.LENGTH_SHORT).show();
+    }
+}
+
